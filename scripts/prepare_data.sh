d07e60054c43672f151191904acb3e5bd3946a76
Thành Hưng <hungnguyenarbeit@gmail.com>
2025-07-21 00:43:03 +0700

    add

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -2,6 +2,7 @@
 CARS196_ROOT='data/cars196/'
 CARS196_DATA='1xWSUMQIME3yqK8_NAdoBzYu8kO8bk4gq'
 
+
 if [[ ! -d "${CARS196_ROOT}" ]]; then
     mkdir -p data/cars196/
     pushd data/cars196/
@@ -13,7 +14,7 @@ if [[ ! -d "${CARS196_ROOT}" ]]; then
 fi
 
 
-CUB_ROOT='data/cub200/'
+CUB_ROOT='data/CUB_200_2011/'
 CUB_DATA='https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz'
 
 
@@ -24,7 +25,6 @@ if [[ ! -d "${CUB_ROOT}" ]]; then
     wget ${CUB_DATA}
     tar -zxf CUB_200_2011.tgz
     rm CUB_200_2011.tgz
-    mv CUB_200_2011 cub200
     popd
 fi
 
@@ -32,7 +32,7 @@ fi
 
 
 
-SOP_ROOT='data/online_products/'
+SOP_ROOT='data/Stanford_Online_Products/'
 SOP_DATA='https://www.dropbox.com/scl/fi/7icj466ds04ex7rd7kxxs/online_products.tar?rlkey=c2tp644h3uzui38tpu3l8z2uq&e=1&dl=0'
 
 
@@ -42,10 +42,30 @@ if [[ ! -d "${SOP_ROOT}" ]]; then
     echo "Downloading Stanford Online Products dataset..."
     wget -O online_products.tar "${SOP_DATA}"
     tar -xvf online_products.tar
-    mv Info_Files images
     rm online_products.tar
+    mv online_products Stanford_Online_Products
     popd
 fi
+
+
+
+
+
+INSHOP_ROOT='data/Inshop_Clothes/'
+INSHOP_DATA='0B7EVK8r0v71pVDZFQXRsMDZCX1Ek'
+
+
+if [[ ! -d "${INSHOP_ROOT}" ]]; then
+    mkdir -p data/
+    pushd data/
+    echo "Downloading INSHOP dataset..."
+    gdown "${INSHOP_DATA}" -O Inshop_Clothes.zip
+    unzip Inshop_Clothes.zip
+    rm Inshop_Clothes.zip
+    popd
+fi
+
+
 
 
 
