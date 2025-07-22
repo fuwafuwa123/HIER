5e655d96e1932b41cf26bc45d2713aca56402f89
Thành Hưng <hungnguyenarbeit@gmail.com>
2025-07-22 22:37:06 +0700

    Update food101.py

--- hier/dataset/food101.py
+++ hier/dataset/food101.py
@@ -15,8 +15,9 @@ class Food101(BaseDataset):
             self.classes = list(range(0, 20))
         else:
             self.classes = list(range(99, 102))
-            
-        dataset = load_dataset("food101", split=mode, streaming=False)
+
+        # BẮT BUỘC phải có decode=False
+        dataset = load_dataset("food101", split=mode, decode=False)
 
         index = 0
         for i, item in enumerate(dataset):
@@ -26,8 +27,8 @@ class Food101(BaseDataset):
 
             try:
                 img_bytes = item['image']['bytes']
-                image = Image.open(io.BytesIO(img_bytes)).convert('RGB')
-                _ = image.size  # test lỗi ảnh
+                image = Image.open(io.BytesIO(img_bytes)).convert("RGB")
+                _ = image.size  # kiểm tra ảnh
             except Exception as e:
                 print(f"[Skip] Error reading image at index {i}: {e}")
                 continue
@@ -37,6 +38,7 @@ class Food101(BaseDataset):
             self.images.append(image)
             index += 1
 
+        # Gọi sau khi ảnh đã load xong
         BaseDataset.__init__(self, self.root, self.mode, self.transform)
 
     def __getitem__(self, index):
