4516b8631756c16c6238dee6a8239b90ca1e5d1a
Thành Hưng <216351960+fuwafuwa123@users.noreply.github.com>
2025-07-23 16:57:21 +0700

    Update food101.py

--- hier/dataset/food101.py
+++ hier/dataset/food101.py
@@ -1,5 +1,4 @@
 from .base import *
-from datasets import load_dataset
 from PIL import Image
 import io
 import warnings
@@ -9,21 +8,38 @@ class Food101(BaseDataset):
         self.root = root + '/food41'
         self.mode = mode
         self.transform = transform
+
+        # Tạo danh sách class theo thứ tự alphabet
+        class_names = sorted(os.listdir(self.root + '/images'))
+        self.class_to_id = {name: idx for idx, name in enumerate(class_names)}
+
+        # Chia lớp theo split thủ công
         if self.mode == 'train':
-            self.classes = range(0,100)
+            self.classes = range(0, 100)
         elif self.mode == 'eval':
-            self.classes = range(100,200)
-        
+            self.classes = range(100, 200)
+      
+        # Khởi tạo các biến
+        self.ys = []
+        self.I = []
+        self.im_paths = []
+
+        # Gọi BaseDataset sau khi có self.root, mode, transform
         BaseDataset.__init__(self, self.root, self.mode, self.transform)
-        # Select correct metadata file based on mode
+
+        # Đọc metadata
         meta_file = 'train.txt' if self.mode == 'train' else 'test.txt'
-        meta_path = os.path.join(self.root, 'meta', 'meta', meta_file)
-        if not os.path.exists(meta_path):
-            raise FileNotFoundError(f"Metadata file not found: {meta_path}")
+        meta_path = self.root + '/meta/meta' + meta_file
+
+       
+
         metadata = open(meta_path)
-        for i, (image_id, class_id, _, path) in enumerate(map(str.split, metadata)):
-            if i > 0:
-                if int(class_id)-1 in self.classes:
-                    self.ys += [int(class_id)-1]
-                    self.I += [int(image_id)-1]
-                    self.im_paths.append(os.path.join(self.root, 'images', path))
+        for i, line in enumerate(metadata):
+            path = line.strip() + '.jpg'  # Ví dụ: 'apple_pie/101251.jpg'
+            class_name = path.split('/')[0]
+            class_id = self.class_to_id[class_name]
+
+            if class_id in self.classes:
+                self.ys += [class_id]
+                self.I += [i]
+                self.im_paths.append(self.root + '/images/' + path)
